55f1b08c02663ab267a3ca9008f276318cd17aca
Vincent Song <yufei_0229@163.com>
2022-03-18 20:40:53 +0800

    Fri Mar 18 08:40:53 PM CST 2022

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,9 @@
-if status is-interactive
+# if status is-interactive
 	set fish_greeting ""
 	set -gx EDITOR nvim
 	set -gx NEOVIDE_MULTIGRID
+	set -xg NEMU_HOME /home/songtx/learn/nj/ics2021/nemu
+	set -xg AM_HOME /home/songtx/learn/nj/ics2021/abstract-machine
 	fish_add_path ~/.local/bin/
 	fish_add_path ~/.emacs.d/bin/
 	fish_add_path ~/.cargo/bin/
@@ -11,7 +13,9 @@ if status is-interactive
 	alias la="exa -la --icons"
 	alias nv="nvim"
 
-	# set -gx ALL_PROXY "socks5://127.0.0.1:20170"
-	# set -gx http "http://127.0.0.1:20171"
-	# set -gx https "https://127.0.0.1:20171"
-end
+	set -gx http_proxy "http://127.0.0.1:7890"
+	set -gx https_proxy "http://127.0.0.1:7890"
+	set -gx socks5_proxy "socks5://127.0.0.1:7891"
+	set -gx redir_proxy "socks5://127.0.0.1:7892"
+	# set -gx all_proxy "socks5://127.0.0.1:7891"
+# end
